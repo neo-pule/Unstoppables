130dab99b3cc5bab4c2b33bf1a2b0f980866ddad
Academy_learners <academy_learners@Academy-learnerss-MacBook-Air.local>
2021-12-02 23:37:47 -0800

    removed comments

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
+
 var days = ['Monday','Tuesday','Thursday','Friday','Saturday','Sunday']
 for(i = 0; i < days.length;i++)
 {
     console.log(days[i]);
 }
-console.log("hello");
-=======
-console.log("hello");
 console.log('this is written by tshepo')
->>>>>>> fd2495c65017d8facd0e21249d503e2fd583009c
