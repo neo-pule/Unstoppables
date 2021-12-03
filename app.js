8f1789bc9a6fbd34d3bbfd28a5b6e533cd0fb71e
Andrica1-GitHub <andricamabusela1@gmail.com>
2021-12-03 14:20:23 +0200

    another commit

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ for(i = 0; i < days.length;i++)
     console.log(days[i]);
 }
 console.log('this is written by tshepo')
-<<<<<<< HEAD
-console.log("hello");
-=======
 console.log('second commit by tshepo')
 console.log("test")
 alert('coding')
->>>>>>> bd634905de0b6479861d912821ffa902789f43d8
+console.log('last line of code');
